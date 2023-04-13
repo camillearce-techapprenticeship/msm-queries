07184e61edd07d1fc4281b632d94c93606127879
Camille Arce <thecamillearce@gmail.com>
2023-04-13 03:55:47 +0000

    finished needed for grading

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,8 +8,10 @@ class ActorsController < ApplicationController
   def actor_details
 
     actor_id = params.fetch("actor_id")
-
     @this_actor = Actor.where({ :id => actor_id }).at(0)
+
+    #this will give me an array where I can do a loop
+    @this_character = Character.where({ :actor_id => actor_id })
 
     render({ :template => "actor_templates/actor_details.html.erb" })
   end
